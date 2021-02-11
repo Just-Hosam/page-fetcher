ef941e4878355db1df18136754a67da2c294f2d8
Hosam Dahrooge <Hosam_firas@hotmail.com>
2021-02-11 10:20:50 +0000

    added logic

--- fetcher.js
+++ fetcher.js
@@ -2,9 +2,13 @@ const request = require('request');
 const fs = require('fs');
 
 let args = process.argv.slice(2);
+console.log(args[1]);
 
 request(`${args[0]}`, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+  fs.writeFile(`${args[1]}`, body, function (err) {
+    if (err) throw err;
+    
+    let fileSize = fs.statSync(`${args[1]}`).size;
+    console.log(`Downloaded and saved ${fileSize} bytes to ${args[1]}`)
+  });
 });
