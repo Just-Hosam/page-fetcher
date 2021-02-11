74db381d554256ab3681f6368076456cdc5f3996
Hosam Dahrooge <Hosam_firas@hotmail.com>
2021-02-11 09:46:29 +0000

    uninstalled fs package

--- fetcher.js
+++ fetcher.js
@@ -1,9 +1,8 @@
 const request = require('request');
-const fs = requires('fs');
+const fs = require('fs');
 
 let args = process.argv.slice(2);
 
-const request = require('request');
 request(`${args[0]}`, (error, response, body) => {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
